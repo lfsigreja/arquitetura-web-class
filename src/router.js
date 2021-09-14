fa81366243870d70126ba01a65e0e160a4e5508d
Luis Igreja <lfsigreja@gmail.com>
2021-09-13 22:21:03 -0300

    reefactorin importing system with sucrase

--- src/router.js
+++ src/router.js
@@ -1,9 +1,9 @@
-const { Router } = require('express');
+import { Router } from 'express';
 
 const routes = new Router();
 
-routes.get('/test', (req, res) => {
+routes.get('', (req, res) => {
   return res.json({ message: 'Okay' });
 });
 
-module.exports = routes;
+export default routes;
